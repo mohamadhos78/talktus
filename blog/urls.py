55412f72875d08563e21cce6f9af85d605e77cb9
mohammadhosein <mohamadhos88@gmail.com>
2021-03-31 02:45:10 +0430

    debuggings, paginations, categories for posts page, last-posts

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.landing , name="landing" ) ,
-    path("posts/<str:category>", views.posts , name="posts" ) ,
-    path("post/<str:title>", views.post , name="post" ) ,
+    path("posts/<str:category>/", views.posts , name="posts" ) ,
+    path("post/<str:title>/", views.post , name="post" ) ,
     path("search/", views.search , name="search" ) ,
+    path("last_posts/",views.last_posts,name="last_posts"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -19,12 +19,20 @@ def landing(request):
     return render(request, "landingPage.html")
 
 def posts(request,category):
+    from django.core.paginator import Paginator
+    from django.db.models import Q
+    page = request.GET.get('page')
+
+    categories = Category.objects.all()
     title = category
     posts = Post.objects.filter(category__title=title)
-    print(len(posts))
+    
+    paginator = Paginator(posts, 10)
+    posts = paginator.get_page(page)
     context = {
         "posts" : posts ,
         "title" : title,
+        "categories" : categories ,
     }
     return render(request, "posts.html",context)
 
@@ -34,16 +42,46 @@ def post(request,title):
     post = get_object_or_404(Post, title=title)
     posts = Post.objects.filter(category__title=post.category.title)
     context = {
-        "post" : post,
-        "posts": posts,
+        "post" : post ,
+        "posts" : posts ,
     }
     return render(request, "text-course.html", context)
 
 def search(request):
+    from django.core.paginator import Paginator
+    from django.db.models import Q
+    page = request.GET.get('page')
+
+    categories = Category.objects.all()
     from django.db.models import Q
     query = request.GET.get('q')
     posts = Post.objects.filter(Q(title__icontains=query) | Q(content__contains=query))
+    title = query
+
+    paginator = Paginator(posts, 10)
+    posts = paginator.get_page(page)
     context = {
        "posts" : posts ,
+       "categories" : categories ,
+       "title" : title ,
     } 
+    return render(request, "posts.html",context)
+
+def last_posts(request):
+    from django.core.paginator import Paginator
+    from django.db.models import Q
+    page = request.GET.get('page')
+
+    categories = Category.objects.all()
+    posts = Post.objects.all().order_by("-date")
+    
+    paginator = Paginator(posts, 10)
+    posts = paginator.get_page(page)
+    
+    title = "آخرین‌ پست‌ها"
+    context = {
+        "posts" : posts ,
+        "title" : title ,
+        "categories" : categories ,
+    }
     return render(request, "posts.html",context)
